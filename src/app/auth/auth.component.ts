3d31a060ab91d9e51ad3a52698f54f111cce6ca5
weiyang wan <weiyangwan@gmail.com>
2017-01-30 12:15:53 +0800

    add scope email to fb login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -43,7 +43,7 @@ export class AuthComponent implements OnInit, AfterViewInit {
   loginFacebook() {
     FB.login((response) =>  {
       this.getDetails();
-    });
+    }, {scope: 'email'});
   }
 
   getDetails() {
