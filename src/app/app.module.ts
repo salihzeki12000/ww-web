65c7109796e85af7bb91755132226ede81a272ba
weiyang wan <weiyangwan@gmail.com>
2017-10-28 14:42:43 +0800

    enable raven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,15 +49,15 @@ import { CitiesComponent, CityService } from './cities';
 import { AutoGrowDirective } from './directives/auto-grow.directive';
 import { AboutUsComponent, ContactUsComponent, PrivacyPolicyComponent } from './basics';
 
-// Raven
-//   .config('https://5575c781e9054ddd828e3e4b8f90bc55@sentry.io/211359')
-//   .install();
-//
-// export class RavenErrorHandler implements ErrorHandler {
-//   handleError(err:any) : void {
-//     Raven.captureException(err);
-//   }
-// }
+Raven
+  .config('https://5575c781e9054ddd828e3e4b8f90bc55@sentry.io/211359')
+  .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err:any) : void {
+    Raven.captureException(err);
+  }
+}
 
 
 @NgModule({
@@ -176,9 +176,7 @@ import { AboutUsComponent, ContactUsComponent, PrivacyPolicyComponent } from './
     routing,
     Daterangepicker
   ],
-  providers: [ Title, AdminService, LoadingService, AuthService, UserService, PostService, ItineraryService, ItineraryEventService, ResourceService, FlashMessageService, RelationshipService, NotificationService, FileuploadService, CommentService, ErrorMessageService, FavouriteService, RecommendationService, PlaceService, CountryService, CityService, AuthGuard, AdminGuard ],
+  providers: [ Title, AdminService, LoadingService, AuthService, UserService, PostService, ItineraryService, ItineraryEventService, ResourceService, FlashMessageService, RelationshipService, NotificationService, FileuploadService, CommentService, ErrorMessageService, FavouriteService, RecommendationService, PlaceService, CountryService, CityService, AuthGuard, AdminGuard, { provide: ErrorHandler, useClass: RavenErrorHandler } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-// , { provide: ErrorHandler, useClass: RavenErrorHandler }
