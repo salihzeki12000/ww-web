ca19aacd43c2c981fbdc00855613992a03edde5a
weiyang wan <weiyangwan@gmail.com>
2017-10-29 14:18:01 +0800

    added navigation for about us, contact us, privacy policy and terms of service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,15 +49,15 @@ import { CitiesComponent, CityService } from './cities';
 import { AutoGrowDirective } from './directives/auto-grow.directive';
 import { AboutUsComponent, ContactUsComponent, PrivacyPolicyComponent, TermsOfServiceComponent } from './basics';
 
-Raven
-  .config('https://5575c781e9054ddd828e3e4b8f90bc55@sentry.io/211359')
-  .install();
-
-export class RavenErrorHandler implements ErrorHandler {
-  handleError(err:any) : void {
-    Raven.captureException(err);
-  }
-}
+// Raven
+//   .config('https://5575c781e9054ddd828e3e4b8f90bc55@sentry.io/211359')
+//   .install();
+//
+// export class RavenErrorHandler implements ErrorHandler {
+//   handleError(err:any) : void {
+//     Raven.captureException(err);
+//   }
+// }
 
 
 @NgModule({
@@ -177,7 +177,9 @@ export class RavenErrorHandler implements ErrorHandler {
     routing,
     Daterangepicker
   ],
-  providers: [ Title, AdminService, LoadingService, AuthService, UserService, PostService, ItineraryService, ItineraryEventService, ResourceService, FlashMessageService, RelationshipService, NotificationService, FileuploadService, CommentService, ErrorMessageService, FavouriteService, RecommendationService, PlaceService, CountryService, CityService, AuthGuard, AdminGuard, { provide: ErrorHandler, useClass: RavenErrorHandler } ],
+  providers: [ Title, AdminService, LoadingService, AuthService, UserService, PostService, ItineraryService, ItineraryEventService, ResourceService, FlashMessageService, RelationshipService, NotificationService, FileuploadService, CommentService, ErrorMessageService, FavouriteService, RecommendationService, PlaceService, CountryService, CityService, AuthGuard, AdminGuard ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// , { provide: ErrorHandler, useClass: RavenErrorHandler }
